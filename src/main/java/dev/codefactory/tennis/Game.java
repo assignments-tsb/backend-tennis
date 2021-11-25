e88fcd5232557749f8aafb655a7b36de5b45e33e
LM Bibera <lyndon.bibera@headhuntr.io>
2021-11-25 16:31:48 +0800

    Implemented the simplified API for Game

--- src/main/java/dev/codefactory/tennis/Game.java
+++ src/main/java/dev/codefactory/tennis/Game.java
@@ -1,19 +1,42 @@
 package dev.codefactory.tennis;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Game {
 
-    private final Map<Player, Integer> currentScore = Map.of(Player.ONE, 0, Player.TWO, 0);
+    private final Map<Player, Integer> currentScore;
+
+    public Game() {
+        currentScore = new HashMap<>();
+        currentScore.put(Player.ONE, 0);
+        currentScore.put(Player.TWO, 0);
+    }
 
     public Integer getCurrentScore(Player player) {
-        return 0;
+        return currentScore.get(player);
     }
 
     public void pointWonBy(Player player) {
+        if (isOver()) return;
+        currentScore.put(player, currentScore.get(player)+1);
+    }
+
+    public boolean isOver() {
+        return playerHasAtLeast4Points() && playerHasScore2PointsMore();
     }
 
     public Player determineWinner() {
-        return null;
+        if (!isOver()) return null;
+        return currentScore.get(Player.TWO) >= 4 ? Player.TWO : Player.ONE;
+    }
+
+    private boolean playerHasAtLeast4Points() {
+        return currentScore.get(Player.ONE) >= 4
+                || currentScore.get(Player.TWO) >= 4;
+    }
+
+    private boolean playerHasScore2PointsMore() {
+        return Math.abs(currentScore.get(Player.ONE) - currentScore.get(Player.TWO)) >= 2;
     }
 }
