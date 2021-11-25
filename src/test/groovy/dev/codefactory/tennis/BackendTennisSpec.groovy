77411c0da23b90904cfab8b67ca4b27df2b12018
LM Bibera <lyndon.bibera@headhuntr.io>
2021-11-25 14:54:44 +0800

    generated the project from template

---
+++ src/test/groovy/dev/codefactory/tennis/BackendTennisSpec.groovy
@@ -0,0 +1,19 @@
+package dev.codefactory.tennis
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import spock.lang.Specification
+import jakarta.inject.Inject
+
+@MicronautTest
+class BackendTennisSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication<?> application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
