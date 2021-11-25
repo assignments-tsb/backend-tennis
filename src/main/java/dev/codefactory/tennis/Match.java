597d68e566324b68e0d5095067a1c85bfed7774f
LM Bibera <lyndon.bibera@headhuntr.io>
2021-11-25 17:43:57 +0800

    Add a test using a table of scores together with the expected state of the game, and winner if possible

--- src/main/java/dev/codefactory/tennis/Match.java
+++ src/main/java/dev/codefactory/tennis/Match.java
@@ -1,16 +1,44 @@
 package dev.codefactory.tennis;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Match {
 
+    private final List<Game> set = new LinkedList<>();
+
     public Game startNewGame() {
-        return new Game();
+        var newGame = new Game();
+        set.add(newGame);
+
+        return newGame;
     }
 
     public boolean isOver() {
-        return true;
+        return onePlayerHasAtLeast6Games() && onePlayerIsAheadBy2();
     }
 
     public Player determineWinner() {
-        return Player.TWO;
+        return countWinsForPlayer(Player.ONE) > countWinsForPlayer(Player.TWO)
+                && isOver()
+                ? Player.ONE : Player.TWO;
+    }
+
+    private boolean onePlayerHasAtLeast6Games() {
+        return countWinsForPlayer(Player.ONE) >= 6 || countWinsForPlayer(Player.TWO) >= 6;
+    }
+
+    private boolean onePlayerIsAheadBy2() {
+        return Math.abs(countWinsForPlayer(Player.ONE)-countWinsForPlayer(Player.TWO)) >= 2;
+    }
+
+    private int countWinsForPlayer(Player player) {
+        int wins = 0;
+
+        for (Game game : set) {
+            if (game.isOver() && game.determineWinner() == player) wins++;
+        }
+
+        return wins;
     }
 }
--- src/test/groovy/dev/codefactory/tennis/MatchSpec.groovy
+++ src/test/groovy/dev/codefactory/tennis/MatchSpec.groovy
@@ -20,4 +20,40 @@ class MatchSpec extends Specification {
         then: "someone wins"
         match.determineWinner() == Player.ONE
     }
+
+    def "a player wins the set by winning at least 6 games"(Integer player1Score, Integer player2Score, Boolean isOver, Player winner) {
+
+        given: "a match"
+        Match match = new Match()
+
+        when: "both player wins X games"
+        winGamesXTimes(match, Player.ONE, player1Score)
+        winGamesXTimes(match, Player.TWO, player2Score)
+
+        then: "the match is over?"
+        match.isOver() == isOver
+
+        and: "player X wins"
+        match.determineWinner() == winner
+
+        where:
+        player1Score | player2Score | isOver | winner
+        0            | 0            | false  | null
+        1            | 0            | false  | null
+        4            | 0            | true   | Player.ONE
+        4            | 3            | false  | null
+        2            | 6            | true   | Player.TWO
+    }
+
+    private static void winGamesXTimes(Match match, Player player, int times) {
+        for (int i=0; i<times; i++) winAGame(match, player)
+    }
+
+    private static void winAGame(Match match, Player winner) {
+        Game game = match.startNewGame()
+        while (!game.isOver()) {
+            game.pointWonBy(winner)
+        }
+    }
+
 }
