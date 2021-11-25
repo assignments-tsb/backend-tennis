e11185579149b38ce689f6d2fdf8da8ecad6d2f8
LM Bibera <lyndon.bibera@headhuntr.io>
2021-11-25 15:51:12 +0800

    Implement the Game behavior for raising the score

--- src/main/java/dev/codefactory/tennis/Game.java
+++ src/main/java/dev/codefactory/tennis/Game.java
@@ -8,16 +8,18 @@ public class Game {
         PLAYER_TWO
     }
 
+    private Score currentScore = Score.LOVE_LOVE();
+
     public Score getCurrentScore() {
-        return Score.LOVE_LOVE();
+        return currentScore;
     }
 
     public void scoreForPlayerOne() {
-
+        currentScore = currentScore.forPlayer1();
     }
 
     public void scoreForPlayerTwo() {
-
+        currentScore = currentScore.forPlayer2();
     }
 
     public Winner pointWonBy() {
--- src/main/java/dev/codefactory/tennis/Score.java
+++ src/main/java/dev/codefactory/tennis/Score.java
@@ -8,6 +8,14 @@ public class Score {
     int player1Points;
     int player2Points;
 
+    public Score forPlayer1() {
+        return Score.of(player1Points+1, player2Points);
+    }
+
+    public Score forPlayer2() {
+        return Score.of(player1Points, player2Points+1);
+    }
+
     public static Score LOVE_LOVE() {
         return new Score(0, 0);
     }
