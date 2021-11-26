39f5a87528534cc1fba821d716fa5d5b341ae4d1
LM Bibera <lyndon.bibera@headhuntr.io>
2021-11-26 08:08:11 +0800

    Implement the tie-breaker game

--- src/main/java/dev/codefactory/tennis/TieBreakerGame.java
+++ src/main/java/dev/codefactory/tennis/TieBreakerGame.java
@@ -2,4 +2,11 @@ package dev.codefactory.tennis;
 
 public class TieBreakerGame extends Game {
 
+    private static final int POINTS_TO_WIN = 7;
+
+    @Override
+    protected boolean playerHasEnoughPointsToWin() {
+        return currentScore.get(Player.ONE) >= POINTS_TO_WIN
+                || currentScore.get(Player.TWO) >= POINTS_TO_WIN;
+    }
 }
